e6b76c060b11a3c326b6bedac3cc55a60e56f885
André Cardoso <andrencardoso51@gmail.com>
2025-03-02 02:05:33 +0000

    função procura os n pixeis mais proximos de serem centro; função terminada, apenas falta adicionar marcaçoes melhores e ponderar outros metodos para encontrar centros

--- detect.py
+++ detect.py
@@ -4,15 +4,9 @@ import os
 def clear():
     os.system('cls')
 
-#       colunhas
-#limhas 0 0 0 0 0 0 0 0 [linhas][colunas]
-#       1 1 0 0 1 1 0 1
-
-#esta é a função que atribui a um pixel a sua pontuaçãode correspondência
+#esta é a função que atribui a um pixel a sua pontuação de correspondência
 #com um centro perfeito
-
-win = 25
-def eval(l, c): #a função funciona bem, conta todos os 2500 pixeis numa janela
+def eval(im, l, c, win):
     points = 0
     #1º QUADRANTE
     for y in range(l - win + 1, l + 1):
@@ -29,41 +23,45 @@ def eval(l, c): #a função funciona bem, conta todos os 2500 pixeis numa janela
         for x in range(c - win + 1, c + 1):
             if im[y][x] == 255:
                 points = points + 1
-    #3º QUADRANTE
+    #4º QUADRANTE
     for y in range(l + 1, l + win + 1):
         for x in range(c + 1, c + win + 1):
             if im[y][x] == 0:
                 points = points + 1
-
     return points
 
-im = cv.imread("im1.jpg")
-im = cv.cvtColor(im, cv.COLOR_BGR2GRAY) #se usar, apagar
+#marca a zona proxima a um pixel mais provavel
+def mark(im, l, c):
+    im[l][c] = 100;im[l][c + 1] = 100
+    im[l + 1][c] = 100;im[l + 1][c + 1] = 100
 
-vsize = im.shape[0]
-hsize = im.shape[1]
+#encontra e marca os centros numa imagem
+def find_centers(im, num_centers, win, ui = 1):
+    vsize = im.shape[0]
+    hsize = im.shape[1]
 
-#guarda as coordenadas do pixel mais provavel de ser centro e da sua pontuação
-#[x,y,pontos]
-best = [0,0,0]
+    centers = []
+    best = [0,0,0]
+    percent = 0
+    count = 0
 
-count = 0
-percent = 0
-#Começar o varrimento na segunda metade da imagem
-#o varrimento é feito pixel
-for l in range(int(vsize/2), vsize - win - 1):
-    for c in range(win, hsize - win - 1):
-        count = count + 1
+    for _ in range(num_centers):
+        best[2] = 0
+        for l in range(int(vsize/2), vsize - win - 1):
+            for c in range(win, hsize - win - 1):
+                if(im[l][c] != 100): #apenas avalia o pixel se n for já um centro
+                    points = eval(im,l,c,win)/((win*2)**2)*100
+                if (points > best[2]):
+                    best = [l, c, points]
+                if (ui):
+                    count = count + 1
+                    percent2 = int((count/(((vsize/2 - win)*(hsize - 2*win))*num_centers)) * 100)
+                    if (percent2 > percent):
+                        clear(); print(best,end=' '); print(str(percent + 2)+"%")
+                        percent = percent2
+        centers.append(best.copy())
+        mark(im, best[0], best[1])
 
-        percent2 = int((count/((vsize/2 - win)*(hsize - 2*win))) * 100)
-        if (percent2 > percent):
-                percent = percent2
-                clear()
-                print(str(percent)+"%")
-
-        points = eval(l,c)
-        if (points > best[2]):
-            best = [l, c, points]
-
-im[best[0], best[1]] = 100
-print(best)
+    print(centers)
+    cv.imwrite("im_centers.jpg", im)
+    return centers
--- test.py
+++ test.py
@@ -1,36 +1,7 @@
-import cv2 as cv
+from detect import *
 
-win = int(50/2)
-
-def eval(im, l, c):
-    points = 0
-    count = 0
-    #1º QUADRANTE
-    for y in range(l - win + 1, l + 1):
-        for x in range(c - win + 1, c + 1):
-            count = count + 1
-            if im[y][x] == 0:
-                points = points + 1
-    #2º QUADRANTE
-    for y in range(l - win + 1, l + 1):
-        for x in range(c + 1, c + win + 1):
-            count = count + 1
-            if im[y][x] == 255:
-                points = points + 1
-    #3º QUADRANTE
-    for y in range(l + 1, l + win + 1):
-        for x in range(c - win + 1, c + 1):
-            count = count + 1
-            if im[y][x] == 255:
-                points = points + 1
-    #4º QUADRANTE
-    for y in range(l + 1, l + win + 1):
-        for x in range(c + 1, c + win + 1):
-            count = count + 1
-            if im[y][x] == 0:
-                points = points + 1
-
-    return (points, count)
-
-im = cv.imread("im1.jpg")
+im = cv.imread("im2.jpg")
 im = cv.cvtColor(im, cv.COLOR_BGR2GRAY) #se usar, apagar
+im = cv.threshold(im, 127, 255, cv.THRESH_BINARY)[1] #se usar, apagar
+
+find_centers(im, 3, 4)
